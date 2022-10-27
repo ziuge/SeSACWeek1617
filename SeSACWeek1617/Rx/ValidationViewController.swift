85eee88f6e4fb07765637e259f67b4913b90c763
ziuge <xwcjwwd@ewhain.net>
2022-10-27 15:41:21 +0900

    1027 수업 코드

---
+++ SeSACWeek1617/Rx/ValidationViewController.swift
@@ -0,0 +1,122 @@
+//
+//  ValidationViewController.swift
+//  SeSACWeek1617
+//
+//  Created by CHOI on 2022/10/27.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class ValidationViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    @IBOutlet weak var stepButton: UIButton!
+    
+    let disposeBag = DisposeBag()
+    let viewModel = ValidationViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        bind()
+        
+    }
+
+    func bind() {
+        
+        viewModel.validText
+            .asDriver() // 여기서 asDriver로 변환 과정이 필요함!
+            .drive(validationLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let validation = nameTextField.rx.text // String? (Optional)
+            .orEmpty // String
+            .map { $0.count >= 8 } // Bool
+            .share() // Subject, Relay
+
+        validation
+            .bind(to: stepButton.rx.isEnabled, validationLabel.rx.isHidden)
+            .disposed(by: disposeBag)
+
+        validation
+            .withUnretained(self)
+            .bind { (vc, value) in
+                let color: UIColor = value ? .systemPink : .lightGray
+                vc.stepButton.backgroundColor = color
+            }
+            .disposed(by: disposeBag)
+        
+        stepButton.rx.tap
+            .bind { _ in
+                print("SHOW ALERT")
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    func observableVSSubject() {
+        
+        let testA = stepButton.rx.tap
+            .map { "안녕하세요" }
+            .asDriver(onErrorJustReturn: "")
+//            .share()
+
+        testA
+//            .bind(to: validationLabel.rx.text)
+            .drive(validationLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        testA
+//            .bind(to: nameTextField.rx.text)
+            .drive(nameTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        testA
+//            .bind(to: stepButton.rx.title())
+            .drive(stepButton.rx.title())
+            .disposed(by: disposeBag)
+        
+        
+        let sampleInt = Observable<Int>.create { observer in
+            observer.onNext(Int.random(in: 1...100))
+            return Disposables.create()
+        }
+     
+        sampleInt.subscribe { value in
+            print("sampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        sampleInt.subscribe { value in
+            print("sampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        sampleInt.subscribe { value in
+            print("sampleInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        
+        let subjectInt = BehaviorSubject(value: 0)
+        subjectInt.onNext(Int.random(in: 1...100))
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+        
+        subjectInt.subscribe { value in
+            print("subjectInt: \(value)")
+        }
+        .disposed(by: disposeBag)
+    }
+}
---
+++ SeSACWeek1617/Rx/ValidationViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ValidationViewModel.swift
+//  SeSACWeek1617
+//
+//  Created by CHOI on 2022/10/27.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ValidationViewModel {
+    
+    // Validation 문구
+    let validText = BehaviorRelay(value: "닉네임은 최소 8자 이상 필요해요")
+    
+    
+}
